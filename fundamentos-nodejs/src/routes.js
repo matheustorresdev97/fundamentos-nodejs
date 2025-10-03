1aa14c1a7780eb6fef96a0f628270daa7579ef7d
matheustorresdev97 <matheustorresdev@gmail.com>
2025-10-03 20:57:42 -0300

    projeto finalizado

--- fundamentos-nodejs/src/routes.js
+++ fundamentos-nodejs/src/routes.js
@@ -5,6 +5,7 @@ import { buildRoutePath } from "./utils/build-route-path.js";
 const database = new Database();
 
 export const routes = [
+  
   {
     method: "GET",
     path: buildRoutePath("/users"),
