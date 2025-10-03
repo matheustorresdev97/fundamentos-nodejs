9f253ae05e03428770a5d71d39601da30827210f
matheustorresdev97 <matheustorresdev@gmail.com>
2025-10-03 20:45:42 -0300

    atualizando registros

--- 01-fundamentos-nodejs/src/utils/build-route-path.js
+++ 01-fundamentos-nodejs/src/utils/build-route-path.js
@@ -1,5 +1,12 @@
 export function buildRoutePath(path) {
   const routeParametersRegex = /:([a-zA-Z]+)/g;
 
-  console.log(Array.from(path.matchAll(routeParametersRegex)));
+  const paramsWithParams = path.replaceAll(
+    routeParametersRegex,
+    "(?<$1>[a-z0-9-_]+)"
+  );
+
+  const pathRegex = new RegExp(`^${paramsWithParams}`);
+
+  return pathRegex;
 }
