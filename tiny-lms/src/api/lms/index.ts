faed14abe7bc44eddb838c9740f70dbafb430a7e
matheustorresdev97 <matheustorresdev@gmail.com>
2026-06-30 07:46:42 -0300

    feat:  inicializar LmsApi com a instância core na inicialização do servidor

---
+++ tiny-lms/src/api/lms/index.ts
@@ -0,0 +1,9 @@
+import { Api } from "../../core/utils/abstract.ts";
+import { lmsTables } from "./tables.ts";
+
+
+export class LmsApi extends Api {
+  tables(): void {
+    this.db.exec(lmsTables);
+  }
+}
--- tiny-lms/src/server.ts
+++ tiny-lms/src/server.ts
@@ -1,6 +1,11 @@
 import { Core } from "./core/core.ts";
 
+import { LmsApi } from "./api/lms/index.ts";
+
 const core = new Core();
+
+new LmsApi(core).init();
+
 
 core.router.get('/', (req, res) => {
   res.status(200).json('ola');
