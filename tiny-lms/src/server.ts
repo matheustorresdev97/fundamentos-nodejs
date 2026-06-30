d583f52a545da59c7e7d5ad4616e7ec4c7b25ba4
matheustorresdev97 <matheustorresdev@gmail.com>
2026-06-30 07:48:13 -0300

    feat: implementar AuthApi com esquema inicial da tabela de usuários e integrar ao núcleo do servidor

--- tiny-lms/src/server.ts
+++ tiny-lms/src/server.ts
@@ -1,11 +1,13 @@
 import { Core } from "./core/core.ts";
 
+import { AuthApi } from "./api/auth/index.ts";
 import { LmsApi } from "./api/lms/index.ts";
+
 
 const core = new Core();
 
+new AuthApi(core).init();
 new LmsApi(core).init();
-
 
 core.router.get('/', (req, res) => {
   res.status(200).json('ola');
