ffef8421764f7d99dc5f1f02ac20faf0f1fb0e2e
matheustorresdev97 <matheustorresdev@gmail.com>
2026-07-01 07:49:51 -0300

    feat: implementar gerenciamento de sessões com armazenamento em banco de dados e autenticação baseada em cookies

--- tiny-lms/src/api/auth/index.ts
+++ tiny-lms/src/api/auth/index.ts
@@ -1,10 +1,12 @@
 import { Api } from '../../core/utils/abstract.ts';
 import { RouteError } from '../../core/utils/route-error.ts';
 import { AuthQuery } from './query.ts';
+import { SessionService } from './services/session.ts';
 import { authTables } from './tables.ts';
 
 export class AuthApi extends Api {
   query = new AuthQuery(this.db);
+  session = new SessionService(this.core);
   handlers = {
     postUser: (req, res) => {
       const { name, username, email, password } = req.body;
@@ -21,7 +23,7 @@ export class AuthApi extends Api {
       }
       res.status(201).json({ title: 'usuário criado' });
     },
-    postLogin: (req, res) => {
+    postLogin: async (req, res) => {
       const { email, password } = req.body;
       const user = this.db
         .query(
@@ -34,7 +36,14 @@ export class AuthApi extends Api {
       if (!user || password !== user.password_hash) {
         throw new RouteError(404, 'email ou senha incorretos');
       }
-      res.setHeader('Set-Cookie', `sid=${user.id}; Path=/`);
+
+      const { sid_hash } = await this.session.create({
+        userId: user.id,
+        ip: req.ip,
+        ua: req.headers['user-agent'] ?? '',
+      });
+
+      res.setHeader('Set-Cookie', `sid=${sid_hash}; Path=/`);
       res.status(200).json('teste');
     },
   } satisfies Api['handlers'];
